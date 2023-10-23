f62d71380eb69ed9ac02612a9d115696d7277dce
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-10-23 19:31:21 +0200

    mobile-page

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/about';
+import './js/community';
+import './js/education-section';
+import './js/example';
+import './js/footer';
+import './js/header';
+import './js/join-section';
+import './js/modal-mobile';
